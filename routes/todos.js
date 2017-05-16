a879b72041f51740bbc5c15ce02701af2fbf4589
Mohammad-alfatih <alfaitha@gmail.com>
2017-05-16 19:07:12 -0400

    a few updates

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://hamada:hamada@ds143071.mlab.com:43071/mean-todos-app', ['todos']);
 
 router.get('/todos', function(req, res, next) {
-    res.send('TODOS API');
+    db.todos.find(function(err, todos) {
+        if(err) {
+            res.send(error);
+        } else {
+            res.json(todos);
+        }
+    });
 });
 
 module.exports = router;
